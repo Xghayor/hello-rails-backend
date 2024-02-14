2a428caddc7ab864c826a19c24544299c9610041
Xghayor <cc.baqir@gmail.com>
2024-02-14 15:36:15 +0100

    update controller

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -2,5 +2,9 @@ class Api::V1::GreetingsController < ApplicationController
   def index
     @greeting = Greeting.random_greeting
     render json: @greeting
+
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
   end
 end
